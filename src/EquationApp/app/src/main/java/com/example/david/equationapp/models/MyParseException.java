dd3962587864755050a9762ee219b33138719a80
David <drdcustomlows@gmail.com>
2017-11-30 12:05:15 -0500

    added comment

--- src/EquationApp/app/src/main/java/com/example/david/equationapp/models/MyParseException.java
+++ src/EquationApp/app/src/main/java/com/example/david/equationapp/models/MyParseException.java
@@ -4,6 +4,9 @@ package com.example.david.equationapp.models;
  * Created by David on 11/28/2017.
  */
 
+/**
+ * this class is a exception i made to throw a parse exception in the PostfixCalculator
+ */
 public class MyParseException extends Exception{
     private String message;
     public MyParseException(String message){
