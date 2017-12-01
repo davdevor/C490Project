8e51fd20251d5639278cb39df43aea5df9cdad32
davdevor <drdcustomlows@gmail.com>
2017-12-01 15:17:26 -0500

    added comments

--- src/EquationApp/app/src/main/java/com/example/david/equationapp/models/DatabaseController.java
+++ src/EquationApp/app/src/main/java/com/example/david/equationapp/models/DatabaseController.java
@@ -40,17 +40,31 @@ public class DatabaseController implements IDatabase {
         mDatabase.addValueEventListener(eventListener);
     }
 
+    /**
+     * removes the event listener
+     */
     @Override
     public void removeValueEventListener(){
         mDatabase.removeEventListener(eventListener);
     }
 
+    /**
+     * this method is supposed to insert the equation into the database
+     * @param e the equation to add
+     * @return boolean if it is succcessful
+     */
     @Override
     public boolean insert(MyEquation e){
             mDatabase.child(e.getName()).setValue(e);
             return true;
     }
 
+    /**
+     * this method updates the equation in the database with the matching name
+     * with the new data in the argument passed
+     * @param e the equation object that holds the new info, but same equation name
+     * @return boolean if successful
+     */
     @Override
     public boolean updateByName(MyEquation e) {
         mDatabase.child(e.getName()).setValue(e);
@@ -58,18 +72,43 @@ public class DatabaseController implements IDatabase {
 
     }
 
+    /**
+     * this mehtod deletes equations by their name from the database
+     * @param e the equation object that holds the equation anme to be deleted
+     * @return boolean if successful
+     */
     @Override
     public boolean deleteByName(MyEquation e) {
         mDatabase.child(e.getName()).removeValue();
         return true;
     }
+
+    /**
+     * this method is used to select all of the relevant equations
+     * @return a map of the equations, the key is a string of the equation name
+     */
     @Override
     public HashMap<String,MyEquation> selectAll() {
+        //list is filled when the database controller is initialized and the event listener is added
+        //because when you add the event listener it acts like data has benn changed so it calls the on
+        //data change method in the DatabaseChange class
         return list;
     }
+
+    /**
+     * this class handles implements the ValueEventListener for the database
+     */
     private class DatabaseChange implements ValueEventListener {
+        /**
+         * this method gets called anytime a data change happens on the level of the database the event
+         * listener is attached to
+         * @param dataSnapshot
+         */
         public void onDataChange(DataSnapshot dataSnapshot) {
+            //clear the list becuase the data in the list could have been changed
             list.clear();
+
+            //for eachloop gets the equations from the datasnap shot and puts them into the list
             for (DataSnapshot equationDataSnapshot : dataSnapshot.getChildren()) {
                 MyEquation equation = equationDataSnapshot.getValue(MyEquation.class);
                 list.put(equation.getName(),equation);
